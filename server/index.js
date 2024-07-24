04dd1fa111f2ee3be1ea28cd8bdf1c010fd250b3
amir Zhaksylykov <amirzhaksylykov@amirs-MacBook-Pro.local>
2024-07-25 02:42:01 +0500

    prettier

--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3500;
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
 app.get("/api/people", (req, res) => {
-    res.status(200).send({
-        success: "true",
-        people: [{
-            id: "1",
-            name: "Amir",
-            age: "19"
-        },
-        {   
-            id: "2",
-            name: "Altair",
-            age: "11"
-        }]
-    });
+  res.status(200).send({
+    success: "true",
+    people: [
+      {
+        id: "1",
+        name: "Amir",
+        age: "19",
+      },
+      {
+        id: "2",
+        name: "Altair",
+        age: "11",
+      },
+    ],
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on ${port}`)
+  console.log(`Server is listening on ${port}`);
 });
